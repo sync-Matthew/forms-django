ff0990e748d1b3b79a936fd8c43f983f63fe4e19
sync-matthew <matthew@synctivate.com>
2025-09-22 07:55:44 -0400

    update schema file name in swagger view

--- form_builder/views.py
+++ form_builder/views.py
@@ -1,6 +1,7 @@
 from api.schema import CustomStatelessJWTAuthenticationScheme
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from drf_spectacular.renderers import OpenApiYamlRenderer
 from drf_spectacular.views import (
     SpectacularSwaggerView,
     SpectacularAPIView,
@@ -15,7 +16,12 @@ class CustomSpectacularSwaggerView(SpectacularSwaggerView):
 
 @method_decorator(login_required, name="dispatch")
 class CustomSpectacularAPIView(SpectacularAPIView):
-    pass
+    renderer_classes = [OpenApiYamlRenderer]
+
+    def finalize_response(self, request, response, *args, **kwargs):
+        response = super().finalize_response(request, response, *args, **kwargs)
+        response["Content-Disposition"] = 'attachment; filename="forms-django.yaml"'
+        return response
 
 
 @method_decorator(login_required, name="dispatch")
